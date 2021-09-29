936a474366ae5fe197e1da76fe89df4a0ad7ec62
Nazima <nazima.saherwala@3disystems.com>
2021-09-29 17:20:29 +0530

    Initial Commit

---
+++ ArtGalleryCenter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtGalleryCenter
+//
+//  Created by Nazima on 9/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
